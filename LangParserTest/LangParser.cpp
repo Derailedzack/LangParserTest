9d82955116a303f3f12ac9a4edb1d8340ad6078e
ShadowFoxy32 <DeRailedZack@hotmail.com>
2022-02-15 18:35:09 -0500

    Remove LangParser class.

--- LangParserTest/LangParser.cpp
+++ LangParserTest/LangParser.cpp
@@ -1,3 +1,4 @@
+/*
 #include "LangParser.h"
 #include <iostream>
 
@@ -8,7 +9,7 @@ LangParser::LangParser()
 
 void LangParser::ParseStr(std::string str_dat, std::string str_in)
 {
-	/*
+	
 	try {
 		tao::pegtl::string_input str_input(str_dat, str_in);
 		tao::pegtl::parse<Grammer>(str_input);
@@ -19,13 +20,14 @@ void LangParser::ParseStr(std::string str_dat, std::string str_in)
 	catch (tao::pegtl::parse_error e) {
 		std::cout << e.what() << std::endl;
 	}
-	*/
+	
 	
 }
 
 void LangParser::ParseFile(std::string path)
 {
-	/*
+	
 	tao::pegtl::file_input file_input(path);
-	*/
+	
 }
+*/
--- LangParserTest/LangParser.h
+++ LangParserTest/LangParser.h
@@ -16,21 +16,23 @@ struct production {
 	static constexpr auto value = lexy::as_string<std::string>;
 
 };
-//#include<pegtl.hpp>
+/*
+#include<pegtl.hpp>
 class LangParser
 {
 private:
 	
 
 
-	/*struct FunDecl : tao::pegtl::one<'func', 'fun'> {};
+	struct FunDecl : tao::pegtl::one<'func', 'fun'> {};
 	struct FunReturnType : tao::pegtl::alpha {};
 	struct FuncImpl : tao::pegtl::must<FunReturnType>, tao::pegtl::opt<FunDecl> {};
-	struct Grammer: tao::pegtl::must<tao::pegtl::must<tao::pegtl::shebang>,FunDecl>{};*/
+	struct Grammer: tao::pegtl::must<tao::pegtl::must<tao::pegtl::shebang>,FunDecl>{};
 public:
 	
 	LangParser();
 	void ParseStr(std::string str_dat, std::string str_in);
 	void ParseFile(std::string path);
 };
+*/
 
